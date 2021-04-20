fc0a6b87af3fb859caf1a0935049c5a9aed794be
pc <pc@pcs-Mac-mini-2.local>
2021-04-20 11:11:41 +0300

    added loadscreen

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from "./src/components/MainScreen"
+import SplashScreen from "react-native-splash-screen"
 
 export default function App() {
+  useEffect(()=>{
+    SplashScreen.hide()
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Main/>
